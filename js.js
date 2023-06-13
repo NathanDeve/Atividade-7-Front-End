d39928b84720737e76cc647d85a5e5541053a9ec
MasterDeve <134635008+NathanDeve@users.noreply.github.com>
2023-06-13 16:14:32 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,89 @@
+var valores = {
+  escreveu: "",
+  escreveu2: "",
+  escreveu3: "",
+  escreveu4: ""
+};
+
+function Limpatxt() {
+  document.getElementById("tci").value = "";
+  document.getElementById("tci2").value = "";
+  document.getElementById("tci3").value = "";
+  document.getElementById("tci4").value = "";
+}
+
+function TCI() {
+  valores.escreveu = document.getElementById("tci").value;
+  Limpatxt();
+}
+
+function Mostrar() {
+  var escrita = document.getElementById("Verificar");
+  escrita.innerHTML = valores.escreveu;
+  Limpatxt();
+}
+
+function TCI2() {
+  valores.escreveu2 = document.getElementById("tci2").value;
+  Limpatxt();
+}
+
+function Mostrar2() {
+  var escrita2 = document.getElementById("Verificar2");
+  escrita2.innerHTML = valores.escreveu2;
+  Limpatxt();
+}
+
+function TCI3() {
+  valores.escreveu3 = document.getElementById("tci3").value;
+  Limpatxt();
+}
+
+function Mostrar3() {
+  var escrita3 = document.getElementById("Verificar3");
+  escrita3.innerHTML = valores.escreveu3;
+  Limpatxt();
+}
+
+function TCI4() {
+  valores.escreveu4 = document.getElementById("tci4").value;
+  Limpatxt();
+}
+
+function Mostrar4() {
+  var escrita4 = document.getElementById("Verificar4");
+  escrita4.innerHTML = valores.escreveu4;
+  Limpatxt();
+}
+
+function LimparValores() {
+  valores = {
+    escreveu: "",
+    escreveu2: "",
+    escreveu3: "",
+    escreveu4: ""
+  };
+  
+  localStorage.removeItem("valores");
+  Limpatxt();
+  document.getElementById("Verificar").innerHTML = "";
+  document.getElementById("Verificar2").innerHTML = "";
+  document.getElementById("Verificar3").innerHTML = "";
+  document.getElementById("Verificar4").innerHTML = "";
+}
+
+window.addEventListener("load", function() {
+  var storedValores = JSON.parse(localStorage.getItem("valores"));
+  if (storedValores) {
+    valores = storedValores;
+  }
+
+  document.getElementById("tci").value = "";
+  document.getElementById("tci2").value = "";
+  document.getElementById("tci3").value = "";
+  document.getElementById("tci4").value = "";
+});
+
+window.addEventListener("beforeunload", function() {
+  localStorage.setItem("valores", JSON.stringify(valores));
+});
